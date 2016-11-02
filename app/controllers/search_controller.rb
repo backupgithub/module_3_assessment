b24ad5e5840e8dc048542a2f2aad9b6945499612
Raphael Barbo <rsbarbo@gmail.com>
2016-11-02 10:18:54 -0600

    update crontroller to return total store founds

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
   def index
     @stores = Store.all_stores_by_zip_code(params[:zipcode])
+    @total_store_found = Store.total_stores_found_by_zipcode(params[:zipcode])
   end
 end
