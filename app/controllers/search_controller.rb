dfcc92cd858f5facb04c0e2b8a14504b83c9f64a
Raphael Barbo <rsbarbo@gmail.com>
2016-11-02 09:35:35 -0600

    create the first method to find all stores by zip code

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,14 @@
 class SearchController < ApplicationController
   def index
+    @stores = Store.find_all_stores_by_zip_code(params[:zipcode])
   end
+end
+
+
+class Store
+
+  def self.find_all_stores_by_zip_code(zipcode)
+    binding.pry
+  end
+
 end
