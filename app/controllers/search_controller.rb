b891ed0ac3ce3bec658b0097aea5c42f0c91299e
Raphael Barbo <rsbarbo@gmail.com>
2016-11-02 09:48:32 -0600

    create Store class with service inside, now it is returning json

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,7 +8,16 @@ end
 class Store
 
   def self.find_all_stores_by_zip_code(zipcode)
-    binding.pry
+    conn = Faraday.new(:url => 'https://api.bestbuy.com/v1/') do |faraday|
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+    response = conn.get do |req|
+      req.url ("stores(area(#{zipcode},25))")
+      req.params['apiKey'] = ENV['bestbuykey']
+      req.params['format'] = 'json'
+      req.params['pageSize'] = '15'
+      req.params['show'] = 'longName,city,distance,phone,storeType,storeId'
+    end
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
   end
-
 end
