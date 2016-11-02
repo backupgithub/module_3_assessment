45fe017677425cf33769d2c9e38da2ff3f1dad8a
Raphael Barbo <rsbarbo@gmail.com>
2016-11-02 09:54:32 -0600

    update controller with splitted methods

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,18 +6,24 @@ end
 
 
 class Store
-
   def self.find_all_stores_by_zip_code(zipcode)
-    conn = Faraday.new(:url => 'https://api.bestbuy.com/v1/') do |faraday|
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-    end
-    response = conn.get do |req|
+    response = connection.get do |req|
       req.url ("stores(area(#{zipcode},25))")
       req.params['apiKey'] = ENV['bestbuykey']
       req.params['format'] = 'json'
       req.params['pageSize'] = '15'
       req.params['show'] = 'longName,city,distance,phone,storeType,storeId'
     end
-    parsed_response = JSON.parse(response.body, symbolize_names: true)
+    parser(response)
+  end
+
+  def self.parser(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.connection
+      Faraday.new(:url => 'https://api.bestbuy.com/v1/') do |faraday|
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
   end
 end
