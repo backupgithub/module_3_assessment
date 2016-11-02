e07a4098d729a6b58de5995dcc3ff5b74727f52a
Raphael Barbo <rsbarbo@gmail.com>
2016-11-02 10:15:34 -0600

    update store with intialized methods for name, city, etc..

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,6 +2,12 @@ class Store
 
   def initialize(store)
     binding.pry
+    @long_name = store[:longName]
+    @city = store[:city]
+    @distance = store[:distance]
+    @phone = store[:phone]
+    @store_type = store[:storeType]
+    @store_id = store[:storeId]
   end
 
   def self.service
