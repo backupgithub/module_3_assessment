419f6b4d5d65ae98615e77f6c21ed9a65f42c0d3
Raphael Barbo <rsbarbo@gmail.com>
2016-11-02 10:35:05 -0600

    update best buy service to return total

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -11,6 +11,17 @@ class BestBuyService
     parser(response)
   end
 
+  def get_total_stores_found_by_zipcode(zipcode)
+    response = connection.get do |req|
+      req.url ("stores(area(#{zipcode},25))")
+      req.params['apiKey'] = ENV['bestbuykey']
+      req.params['format'] = 'json'
+      req.params['pageSize'] = '15'
+      req.params['show'] = 'longName,city,distance,phone,storeType,storeId'
+    end
+    parser(response)
+  end
+
   private
 
   def parser(response)
